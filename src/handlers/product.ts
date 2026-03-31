c4f81b54b75f712175f675d8844ece85000cc8e3
paul1n4 <paulinevaleroc@gmail.com>
2026-03-31 12:52:22 -0400

    Validate product in handler using express-validator

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express"
+import { check, validationResult } from "express-validator";
 import Product from "../models/Product.model";
 
 export const createProduct = async (req : Request, res : Response) => {
 
-  const product = new Product(req.body)
-  const savedProduct = await product.save()
   
+  //Validación
+  await check('name')
+    .notEmpty().withMessage('Product name cannot be empty')
+    .run(req)
+  await check('price')
+    .isNumeric().withMessage('Invalid value')
+    .notEmpty().withMessage('Product price cannot be empty')
+    .custom(value => value > 0).withMessage('Invalid price')
+    .run(req)
 
-  res.json({data: savedProduct})
+  let errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  const product = await Product.create(req.body)
+  res.json({data: product})
 }
